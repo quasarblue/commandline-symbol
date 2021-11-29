4f69c73738a293f6b75af326d1ed14daf0f8fede
quasarblue <quazarblue@gmail.com>
2021-11-29 11:42:00 +0100

    package typo

---
+++ src/main/java/ninja/symbolnode/account/App.java
@@ -0,0 +1,13 @@
+package ninja.symbolnode.account;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
